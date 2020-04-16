e2b4df7f61fa09f31a68f23f696424c21b8e5efd
Luke-Sparrow <63018715+Luke-Sparrow@users.noreply.github.com>
2020-04-16 13:33:23 -0600

    Add files via upload
    Updated functionality to include cursor change on hover, strikethrough on li click, and a delete button that removes the parent and deletes the item from the list.

--- script2.js
+++ script2.js
@@ -1,14 +1,50 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.getElementById("masterlist");
+var li = document.getElementsByClassName("item");
+var deleteButton = document.getElementsByClassName("delete");
 
 var inputLength = input.value.length;
 
+
+// Adding the remove parent action to existing Delete buttons
+function removeParent(event) {
+	event.target.removeEventListener("click", removeParent, false);
+	event.target.parentNode.remove();
+}
+
+for (var i = deleteButton.length - 1; i >= 0; i--) {
+	deleteButton[i].addEventListener("click", removeParent, false);
+}
+
+// Toggling the "done" class since the var li looks for the item class
+
+function getEventTarget(e){
+	e = e || window.event;
+	return e.target || e.srcElement;
+}
+
+ul.onclick = function(event){
+	var target = getEventTarget(event);
+	target.classList.toggle("done");
+}
+
+
+
+
+// adding new items to the list on enter press or button click
 function createListItem(){
+	var btn = document.createElement("button");
+	btn.innerHTML = "Delete";
+	btn.onclick = removeParent;
+
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
+	li.appendChild(btn);
+	li.classList.add("item");
 	ul.appendChild(li);
-	console.log(input.value);
+	// Use below to test that the event listener is working
+	// console.log(input.value);
 	input.value = "";
 }
 
@@ -28,3 +64,5 @@ function addAfterEnter(event) {
 button.addEventListener("click", addAfterClick);
 
 input.addEventListener("keypress", addAfterEnter);
+
+// li.addEventListener("click", toggleDone);
